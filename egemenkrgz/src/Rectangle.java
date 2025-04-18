dae42f44f988279a5404bba461ae7cd24de07bcf
EgemenKrgz0 <mehmetegemenkaragoz@posta.mu.edu.tr>
2025-04-18 12:15:27 +0300

    week8-EgemenKrgz0

---
+++ egemenkrgz/src/Rectangle.java
@@ -0,0 +1,54 @@
+public class Rectangle extends Shape {
+
+
+    private int sideA = 0, sideB = 0;
+
+    public Rectangle(){
+
+    }
+    public Rectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public Rectangle(String color, int sideA, int sideB) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(int sideB) {
+        this.sideB = sideB;
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(int sideA) {
+        this.sideA = sideA;
+    }
+
+    @Override
+    public void describe() {
+        super.describe();
+        System.out.println("Side A " + sideA +
+                " Side B " + sideB);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Rectangle rectangle = (Rectangle) obj;
+        if(this.sideA== rectangle.sideA
+        && this.sideB == rectangle.sideB
+        && this.getColor().equals(rectangle.getColor())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
